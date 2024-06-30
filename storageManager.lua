c85694480c067531d0f46c549a9bee34bdfc0315
Josh <dev@cldv.org>
2024-06-30 20:58:14 +0200

    add configurations add run method with rednet host

--- storageManager.lua
+++ storageManager.lua
@@ -1,47 +1,66 @@
 local storageManager = {}
+local scm = require("./scm")
+local Config = scm:load("config")
 local args = {...}
 
----@param peripherals table { alias: peripheralName }
-function storageManager:init()
-    -- Wrap all available peripherals
-    local peripherals = peripheral.getNames()
-    self.peripherals = {}
-    for i=1, #peripherals do
-        self.peripherals[peripherals[i]] = peripheral.wrap(peripherals[i])
-    end
+local defaultConfig = {
+    ["protocol"] = {
+        ["description"] = "Protocol used for rednet.",
+        ["default"] = "00",
+        ["type"] = "string"
+    },
+    ["protocolPrefix"] = {
+        ["description"] = "Prefix used for the rednet protocol.",
+        ["default"] = "storageManager#",
+        ["type"] = "string"
+    },
+    ["serverName"] = {
+        ["description"] = "Name of the server used in rednet communication.",
+        ["default"] = "storageManager",
+        ["type"] = "string"
+    },
+}
 
-    -- Create a startup file for the program, wont override existing file
+function storageManager:init()
+    Config:init(defaultConfig)
+
     if args[1] == "startup" then
         self:addToStartup()
+    elseif args[1] == "config" then
+        Config:command(args)
+    else
+        -- Wrap all available peripherals
+        local peripherals = peripheral.getNames()
+        self.peripherals = {}
+        for i=1, #peripherals do
+            self.peripherals[peripherals[i]] = peripheral.wrap(peripherals[i])
+        end
+
+        self:run()
     end
-
-    -- Map of items
-    -- self.items[name] = {
-    --    [slot] = count
-    --    ...
-    -- }
-    self.items = nil
-
-    self:prepareCommands()
-    self:fetchItems()
 end
 
+---@param peripheral string
+---@return table
 function storageManager:fetchItems(peripheral)
+    local items = {}
     for slot, item in pairs(self.peripherals[peripheral].list()) do
-        self.items[item.name] = {
+        items[item.name] = {
             [slot] = item.count,
         }
     end
+
+    return items
 end
 
 ---@param from string peripheral name
 ---@param to string peripheral name
----@param name string item name (with prefix e.g.: `minecraft:`)
+---@param fromSlot number slot to take item from
 ---@param count number | nil optional
-function storageManager:pullItem(from, to, name, count)
+---@param toSlot number slot to put item to
+function storageManager:pullItem(from, to, fromSlot, count, toSlot)
     count = count or 1
-    local slot = self.items[name].slot
-    self.peripherals[to].pullItems(self.peripherals[from].name, slot, count)
+    self.peripherals[to].pullItems(self.peripherals[from].name, fromSlot, count, toSlot)
 end
 
 ---@param from string peripheral name
@@ -58,27 +77,6 @@ function storageManager:pushItem(from, to, slot, count)
     end
 end
 
--- define command names for API
-function storageManager:prepareCommands()
-    self.commands = {
-        ["extract"] = self.pullItem,
-        ["list"] = self.fetchItems,
-        ["search"] = self.search,
-        ["put"] = self.pushItem,
-    }
-end
-
----@param name string
----@return table | nil
-function storageManager:search(name)
-    for _name, item in pairs(self.items) do
-        if _name.find(name) then
-            return item
-        end
-    end
-    return nil
-end
-
 function storageManager:addToStartup()
     local startupPath = "./startup.lua"
     if not fs.exists(startupPath) then
@@ -88,6 +86,70 @@ function storageManager:addToStartup()
     else
         print("startup.lua already exists.")
     end
+end
+
+function storageManager:run()
+    local protocol = Config:get('protocolPrefix') .. Config:get('protocol')
+    local name = Config:get('serverName')
+    rednet.host(protocol, name)
+    print("Server `" .. name .. "` running on protocol `" .. protocol .. "`")
+    
+    while true do
+        local deviceID
+        local message
+        deviceID, message, _ = rednet.receive(protocol)
+        local messageObj = textutils.unserialiseJSON(message)
+        local command = messageObj["command"]
+        local response = ""
+        local success = false
+        
+        if command == "list" then
+            local peripheral = messageObj["peripheral"]
+            local items = nil
+            if peripheral then
+                items = self:fetchItems(peripheral)
+            end
+            if items then success = true end
+
+            response = textutils.serialiseJSON({
+                ["success"] = success,
+                ["items"] = items
+            })
+        elseif command == "put" then
+            local from = messageObj["from"]
+            local to = messageObj["to"]
+            local slot = messageObj["slot"]
+            local count = messageObj["count"]
+            
+            if from and to and slot then
+                self:pushItem(from, to, slot, count)
+                success = true
+            end
+
+            response = textutils.serialiseJSON({
+                ["success"] = success
+            })
+        elseif command == "extract" then
+            local from = messageObj["from"]
+            local to = messageObj["to"]
+            local fromSlot = messageObj["fromSlot"]
+            local count = messageObj["count"]
+            local toSlot = messageObj["toSlot"]
+
+            if from and to and fromSlot and count then
+                self:pullItem(from, to, fromSlot, count, toSlot)
+                success = true
+            end
+            
+            response = textutils.serialiseJSON({
+                ["success"] = success
+            })
+        end
+
+        rednet.send(deviceID, response, protocol)
+    end
+
+    rednet.unhost(protocol)
 end
 
 storageManager:init()
